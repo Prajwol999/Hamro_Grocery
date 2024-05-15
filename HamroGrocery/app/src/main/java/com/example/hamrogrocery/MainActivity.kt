df4a20654f7f0edb091aba5c24f106cd939feebb
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-05-15 22:14:01 +0545

    Splash screen Completed

--- HamroGrocery/app/src/main/java/com/example/hamrogrocery/MainActivity.kt
+++ HamroGrocery/app/src/main/java/com/example/hamrogrocery/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.hamrogrocery
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.hamrogrocery.databinding.ActivityMainBinding
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
     lateinit var  mainBinding: ActivityMainBinding
@@ -14,5 +17,12 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         mainBinding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent= Intent(this@MainActivity,LandingActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+
+        }
     }
-}
