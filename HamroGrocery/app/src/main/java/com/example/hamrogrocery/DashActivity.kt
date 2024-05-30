87cf3985108a45650135e40c30dc0781f06e5166
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-05-30 18:12:33 +0545

    Viewpager in dash board instead of Framelayout using adapter for swipe

--- HamroGrocery/app/src/main/java/com/example/hamrogrocery/DashActivity.kt
+++ HamroGrocery/app/src/main/java/com/example/hamrogrocery/DashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hamrogrocery
 
+import android.content.Intent
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.MenuItem
@@ -9,6 +10,8 @@ import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.viewpager2.widget.ViewPager2
+import com.example.hamrogrocery.adapters.DashViewPagerAdapter
 import com.example.hamrogrocery.databinding.ActivityDashBinding
 import com.example.hamrogrocery.fragments.HistoryFragment
 import com.example.hamrogrocery.fragments.HomeFragment
@@ -19,195 +22,103 @@ import com.google.android.material.navigation.NavigationView
 
 class DashActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var dashBinding: ActivityDashBinding
+    lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        class DashActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-            lateinit var dashBinding: ActivityDashBinding
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                dashBinding = ActivityDashBinding.inflate(layoutInflater)
-                setContentView(dashBinding.root)
-
-                setSupportActionBar(dashBinding.toolbar)
-                val toggle= ActionBarDrawerToggle(this,dashBinding.drawerlayout,dashBinding.toolbar,R.string.nav_open,R.string.nav_close)
-                dashBinding.drawerlayout.addDrawerListener(toggle)
-                toggle.syncState()
-                dashBinding.navigationDrawer.setNavigationItemSelectedListener(this)
-
-
-                dashBinding.navMenu.setOnItemSelectedListener {
-                    when(it.itemId){
-                        R.id.home ->{ replaceFragment(HomeFragment())
-                            dashBinding.navMenu.itemTextAppearanceActive
-                        }
-                        R.id.shop ->{replaceFragment(ShopFragment())
-                            dashBinding.navMenu.itemTextAppearanceActive
-                        }
-                        R.id.list -> {
-                            replaceFragment(ListFragment())
-                            dashBinding.navMenu.itemTextAppearanceActive
-                        }
-                        R.id.history -> {
-                            replaceFragment(HistoryFragment())
-                            dashBinding.navMenu.itemTextAppearanceActive
-                        }
-                        R.id.profile -> {
-                            replaceFragment(ProfileFragment())
-                            dashBinding.navMenu.itemTextAppearanceActive
-                        }
-                        else -> {}
-                    }
-
-
-                    true
-                }
-                val colorStateList = ColorStateList(
-                    arrayOf(
-                        intArrayOf(android.R.attr.state_selected), // State when the item is selected
-                        intArrayOf(-android.R.attr.state_selected) // State when the item is not selected
-                    ),
-                    intArrayOf(
-                        resources.getColor(R.color.grey),
-                        resources.getColor(R.color.white)
-                    ))
-
-                dashBinding.navMenu.itemIconTintList = colorStateList
-                dashBinding.navMenu.itemTextColor = colorStateList
-            }
-
-
-            private fun replaceFragment(fragment: Fragment) {
-                val fragmentManager: FragmentManager =supportFragmentManager
-                val fragmentTransaction: FragmentTransaction =fragmentManager.beginTransaction()
-
-                fragmentTransaction.replace(R.id.frameLayout,fragment)
-                fragmentTransaction.commit()
-
-            }
-
-            override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                when(item.itemId){
-                    R.id.profile ->{ replaceFragment(ProfileFragment())
-                        dashBinding.navMenu.itemTextAppearanceActive
-                    }
-                    R.id.settings -> {
-                        replaceFragment(HistoryFragment())
-                        dashBinding.navMenu.itemTextAppearanceActive
-                    }
-
-                }
-                dashBinding.drawerlayout.closeDrawer(GravityCompat.START)
-                return true
-            }
-
-            override fun onBackPressed() {
-                if (dashBinding.drawerlayout.isDrawerOpen(GravityCompat.START)){
-                    dashBinding.drawerlayout.closeDrawer(GravityCompat.START)
-                }
-                else{
-                    super.onBackPressedDispatcher.onBackPressed()
-                }
-
-
-            }
-
-
-        }
         dashBinding = ActivityDashBinding.inflate(layoutInflater)
         setContentView(dashBinding.root)
 
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = DashViewPagerAdapter(this)
+
         setSupportActionBar(dashBinding.toolbar)
-        val toggle= ActionBarDrawerToggle(this,dashBinding.drawerlayout,dashBinding.toolbar,R.string.nav_open,R.string.nav_close)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            dashBinding.drawerlayout,
+            dashBinding.toolbar,
+            R.string.nav_open,
+            R.string.nav_close
+        )
         dashBinding.drawerlayout.addDrawerListener(toggle)
         toggle.syncState()
         dashBinding.navigationDrawer.setNavigationItemSelectedListener(this)
 
-
         dashBinding.navMenu.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.home ->{ replaceFragment(HomeFragment())
-                dashBinding.navMenu.itemTextAppearanceActive
-                }
-                R.id.shop ->{replaceFragment(ShopFragment())
-                    dashBinding.navMenu.itemTextAppearanceActive
-                }
-                R.id.list -> {
-                    replaceFragment(ListFragment())
-                    dashBinding.navMenu.itemTextAppearanceActive
-                }
-                R.id.history -> {
-                    replaceFragment(HistoryFragment())
-                    dashBinding.navMenu.itemTextAppearanceActive
-                }
-                R.id.profile -> {
-                    replaceFragment(ProfileFragment())
-                    dashBinding.navMenu.itemTextAppearanceActive
-                }
+            when (it.itemId) {
+                R.id.home -> replaceFragment(0)
+                R.id.shop -> replaceFragment(1)
+                R.id.list -> replaceFragment(2)
+                R.id.history -> replaceFragment(3)
+                R.id.profile -> replaceFragment(4)
                 else -> {}
             }
 
-
             true
         }
+
         val colorStateList = ColorStateList(
             arrayOf(
-                intArrayOf(android.R.attr.state_selected), // State when the item is selected
-                intArrayOf(-android.R.attr.state_selected) // State when the item is not selected
+                intArrayOf(android.R.attr.state_selected),
+                intArrayOf(-android.R.attr.state_selected)
             ),
             intArrayOf(
                 resources.getColor(R.color.grey),
                 resources.getColor(R.color.white)
-        ))
+            )
+        )
 
         dashBinding.navMenu.itemIconTintList = colorStateList
         dashBinding.navMenu.itemTextColor = colorStateList
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                dashBinding.navMenu.menu.getItem(position).isChecked = true
+            }
+        })
     }
 
-
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager: FragmentManager =supportFragmentManager
-        val fragmentTransaction: FragmentTransaction =fragmentManager.beginTransaction()
-
-        fragmentTransaction.replace(R.id.frameLayout,fragment)
-        fragmentTransaction.commit()
-
+    private fun replaceFragment(position: Int) {
+        viewPager.currentItem = position
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.profile ->{ replaceFragment(ProfileFragment())
-                dashBinding.navMenu.itemTextAppearanceActive
-            }
-            R.id.settings -> {
-                replaceFragment(HistoryFragment())
+        when (item.itemId) {
+            R.id.profile -> {
+                replaceFragment(4)
                 dashBinding.navMenu.itemTextAppearanceActive
             }
 
+            R.id.settings -> {
+                replaceFragment(3)
+                dashBinding.navMenu.itemTextAppearanceActive
+            }
         }
+
         dashBinding.drawerlayout.closeDrawer(GravityCompat.START)
         return true
     }
 
     override fun onBackPressed() {
-        if (dashBinding.drawerlayout.isDrawerOpen(GravityCompat.START)){
+        if (dashBinding.drawerlayout.isDrawerOpen(GravityCompat.START)) {
             dashBinding.drawerlayout.closeDrawer(GravityCompat.START)
-        }
-        else{
+        } else {
             super.onBackPressedDispatcher.onBackPressed()
         }
-
-
     }
 
-    override fun onPause() {
-        super.onPause()
-        dashBinding.navMenu.itemTextAppearanceActive
+    private fun swipeLeft() {
+        val nextPosition = (viewPager.currentItem + 1) % 5
+        replaceFragment(nextPosition)
     }
 
-    override fun onResume() {
-        super.onResume()
-        dashBinding.navMenu.itemTextAppearanceActive
+    private fun swipeRight() {
+        val previousPosition = (viewPager.currentItem - 1 + 5) % 5
+        replaceFragment(previousPosition)
     }
-
 }
+
+
+
+
