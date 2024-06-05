69ce2b2f9a2d517ef511ddeaa06ee5e45958203b
AakashShahi <129369974+AakashShahi@users.noreply.github.com>
2024-06-05 10:57:39 +0545

    Vendor signup backend and image storage backend sucessful

--- HamroGrocery/app/build.gradle.kts
+++ HamroGrocery/app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
@@ -58,5 +59,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation ("me.relex:circleindicator:1.3.2")
     implementation ("me.relex:circleindicator:2.1.6")
+    implementation("com.squareup.picasso:picasso:2.71828")
 
 }
